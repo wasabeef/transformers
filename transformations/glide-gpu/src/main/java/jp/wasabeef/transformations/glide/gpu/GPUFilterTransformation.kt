bec1130aa627e5330942611df1b9e4738e43958c
wasabeef <dadadada.chop@gmail.com>
2020-09-20 22:43:27 +0900

    update: package

--- transformations/glide-gpu/src/main/java/jp/wasabeef/transformations/glide/gpu/GPUFilterTransformation.kt
+++ transformations/glide-gpu/src/main/java/jp/wasabeef/transformations/glide/gpu/GPUFilterTransformation.kt
@@ -3,14 +3,11 @@ package jp.wasabeef.transformations.glide.gpu
 /**
  * Copyright (C) 2020 Wasabeef
  *
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *
  * http://www.apache.org/licenses/LICENSE-2.0
- *
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,12 +22,13 @@ import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool
 import jp.co.cyberagent.android.gpuimage.GPUImage
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageFilter
 import jp.wasabeef.transformations.glide.BitmapTransformation
+import jp.wasabeef.transformations.glide.gpu.BuildConfig.Version
 import java.security.MessageDigest
 
 abstract class GPUFilterTransformation(private val filter: GPUImageFilter) :
   BitmapTransformation() {
 
-  val version: String = jp.wasabeef.transformations.glide.BuildConfig.Version
+  val version: String = Version
 
   protected val id: String
     get() = "${this::class.java.name}-${version}"
