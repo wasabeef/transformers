29512b3abdbfdc89a0e0f1060d34c54c3c5b3595
wasabeef <dadadada.chop@gmail.com>
2021-10-08 17:37:04 +0900

    chore: remove deprecated param

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,16 +4,11 @@ buildscript {
   repositories {
     google()
     mavenCentral()
-    jcenter()
   }
 
   dependencies {
-    classpath("com.android.tools.build:gradle:4.2.0-beta04")
+    classpath("com.android.tools.build:gradle:7.0.2")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Libraries.kotlinVersion}")
-
-    // TODO: Close JCenter on May 1st https://jfrog.com/blog/into-the-sunset-bintray-jcenter-gocenter-and-chartcenter/
-//    classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
-//    classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
   }
 }
 
@@ -21,7 +16,6 @@ allprojects {
   repositories {
     google()
     mavenCentral()
-    jcenter()
   }
   tasks.withType(Javadoc::class) {
     enabled = false
